57db0c41d41162e58d851d0ddae0076139d99c9b
Garrett Landon <gtlandon@yahoo.com>
2022-04-15 22:32:33 -0400

    Established code base using boilerplate

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const HYPERBOREA = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ HYPERBOREA.abilities = {
+  "str": "HYPERBOREA.AbilityStr",
+  "dex": "HYPERBOREA.AbilityDex",
+  "con": "HYPERBOREA.AbilityCon",
+  "int": "HYPERBOREA.AbilityInt",
+  "wis": "HYPERBOREA.AbilityWis",
+  "cha": "HYPERBOREA.AbilityCha"
+};
+
+HYPERBOREA.abilityAbbreviations = {
+  "str": "HYPERBOREA.AbilityStrAbbr",
+  "dex": "HYPERBOREA.AbilityDexAbbr",
+  "con": "HYPERBOREA.AbilityConAbbr",
+  "int": "HYPERBOREA.AbilityIntAbbr",
+  "wis": "HYPERBOREA.AbilityWisAbbr",
+  "cha": "HYPERBOREA.AbilityChaAbbr"
+};
